def19757e7df8a6260eb2f9e7923e440d018d6ee
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-22 08:53:52 +0200

    add need active session

--- src/Middlewares/Middleware.php
+++ src/Middlewares/Middleware.php
@@ -1,21 +1,25 @@
 <?php
 
 namespace Tualo\Office\QRCodeJS\Middlewares;
+
 use Tualo\Office\Basic\TualoApplication as App;
 use Tualo\Office\Basic\IMiddleware;
 
-class Middleware implements IMiddleware{
-    public static function register(){
-        App::use('QRCodeJS',function(){
-            
-            try{
+class Middleware implements IMiddleware
+{
+    public static function register()
+    {
+        App::use('QRCodeJS', function () {
+
+            try {
                 // require.config({ paths: { 'bcrypt': './papervotelibrary/bcrypt.js' }});
-                App::javascript('qrcodejs_lib', './jsqrcodejs-lib/qrcode.min.js',[],0);
-            }catch(\Exception $e){
-                App::set('maintanceMode','on');
+                App::javascript('qrcodejs_lib', './jsqrcodejs-lib/qrcode.min.js', [], 0);
+            } catch (\Exception $e) {
+                App::set('maintanceMode', 'on');
                 App::addError($e->getMessage());
             }
-        
-        },-100);
+        }, -100, [
+            'needActiveSession' => true,
+        ]);
     }
 }
